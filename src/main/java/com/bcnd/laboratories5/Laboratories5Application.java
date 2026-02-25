9f9c37d75fbd1e1e5a44535d87f068d950a3b423
Abylai <abylai00076@gmail.com>
2026-02-25 10:33:50 +0500

    first massage

---
+++ src/main/java/com/bcnd/laboratories5/Laboratories5Application.java
@@ -0,0 +1,13 @@
+package com.bcnd.laboratories5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratories5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratories5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcnd/laboratories5/Laboratories5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnd.laboratories5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratories5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
